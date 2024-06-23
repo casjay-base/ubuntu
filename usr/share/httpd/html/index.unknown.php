8a0cca5a44f987cb5880329b99c4d0042d808f8c
casjay <git-admin@casjaysdev.pro>
2024-06-23 10:50:03 -0400

    🗃️ Committing everything that changed 🗃️
    index.unknown.php

--- usr/share/httpd/html/index.unknown.php
+++ usr/share/httpd/html/index.unknown.php
@@ -19,20 +19,17 @@
     </div>
     <div class="c5">
       <br />
-      <?php echo "Server Name: " . $_SERVER['SERVER_NAME'] . "<BR>"; echo "IP
-      Address: " . $_SERVER['SERVER_ADDR'] . "<BR />"; ?> <br /><br />
-      Linux OsVer:
-      <?php echo shell_exec('cat /etc/casjaysdev/updates/versions/osversion.txt'); ?><br />
-      ConfigVer:
-      <?php echo shell_exec('cat /etc/casjaysdev/updates/versions/configs.txt'); ?>
+      <?php
+      echo "System Hostname: " , gethostname() . "<br />";
+      echo "Server Name: " . $_SERVER['SERVER_NAME'] . "<br />";
+      echo "IP Address: " . $_SERVER['SERVER_ADDR'] . "<br />";
+      ?>
+      <br /><br />
+      Linux OsVer: <?php echo shell_exec('cat /etc/casjaysdev/updates/versions/osversion.txt'); ?><br />
+      ConfigVer: <?php echo shell_exec('cat /etc/casjaysdev/updates/versions/configs.txt'); ?>
       <br /><br />
       Powered by a Redhat based system<br />
-      <a href="https://redhat.com">
-        <img
-          border="0"
-          alt="Redhat/CentOS/Fedora/SL Linux"
-          src="/default-icons/powered_by_redhat.jpg"
-        /> </a
+      <a href="https://redhat.com"><img border="0" alt="Redhat/CentOS/Fedora/SL Linux" src="/default-icons/powered_by_redhat.jpg"/> </a
       ><br />
       <br /><br /><br /><br /><br />
     </div>
